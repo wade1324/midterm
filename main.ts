52fa7fb80849706eb7dd13ad57f9b2289c379242
wade1324 <71689483+wade1324@users.noreply.github.com>
2020-11-24 14:24:33 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . . . . .
+        . # . # .
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+})
+basic.forever(function () {
+	
+})
